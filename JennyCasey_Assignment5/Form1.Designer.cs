3cc30952fa3612945389be9516a3271ba845f74d
Jenny Paul <jenny@LAPTOP-PQ2SB0U6>
2021-03-23 15:59:11 -0500

    issues with last project, new Assignment 5 repo here

---
+++ JennyCasey_Assignment5/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace JennyCasey_Assignment5
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.gameDifficultyDropDown = new System.Windows.Forms.ComboBox();
+            this.difficultyToolTip = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1371, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(171, 92);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1030, 503);
+            this.canvas.TabIndex = 1;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(139, 0);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(127, 32);
+            this.newGameButton.TabIndex = 2;
+            this.newGameButton.Text = "Start New Puzzle";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.newGameButton_MouseDown);
+            this.newGameButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.newGameButton_MouseUp);
+            // 
+            // gameDifficultyDropDown
+            // 
+            this.gameDifficultyDropDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameDifficultyDropDown.FormattingEnabled = true;
+            this.gameDifficultyDropDown.Location = new System.Drawing.Point(12, 2);
+            this.gameDifficultyDropDown.Name = "gameDifficultyDropDown";
+            this.gameDifficultyDropDown.Size = new System.Drawing.Size(121, 28);
+            this.gameDifficultyDropDown.TabIndex = 3;
+            this.difficultyToolTip.SetToolTip(this.gameDifficultyDropDown, "Please select a difficulty for the puzzle!");
+            this.gameDifficultyDropDown.SelectedIndexChanged += new System.EventHandler(this.gameDifficultyDropDown_SelectedIndexChanged);
+            // 
+            // difficultyToolTip
+            // 
+            this.difficultyToolTip.AutomaticDelay = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 698);
+            this.Controls.Add(this.gameDifficultyDropDown);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Jenny and Casey\'s Sudoku Game";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.ComboBox gameDifficultyDropDown;
+        private System.Windows.Forms.ToolTip difficultyToolTip;
+
+        
+    }
+}
+
---
+++ JennyCasey_Assignment5/Form1.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace JennyCasey_Assignment5
+{
+    public partial class Form1 : Form
+    {
+        private static bool isEasyGame = false;
+        private static bool isMediumGame = false;
+        private static bool isHardGame = false;
+        private static bool isDown = false;
+
+        private static List<TextBox> generatedEasyTextboxes = new List<TextBox>();
+
+        public static int L;
+        public static int W;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //set the length and width of the canvas so less to type later
+            L = canvas.Height;
+            W = canvas.Width;
+
+
+            //add options to the drop down
+            gameDifficultyDropDown.Items.Add("Easy");
+            gameDifficultyDropDown.Items.Add("Medium");
+            gameDifficultyDropDown.Items.Add("Hard");
+
+            resetEasyPuzzleTextboxes();
+        }
+
+        private void gameDifficultyDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //depending what difficulty the user entered, we need to draw the corresponding playing field
+            if (gameDifficultyDropDown.Text == "Easy")
+            {
+                isEasyGame = true;
+            }
+            else if (gameDifficultyDropDown.Text == "Medium")
+            {
+                isMediumGame = true;
+
+            }
+            else if (gameDifficultyDropDown.Text == "Hard")
+            {
+                isHardGame = true;
+            }
+        }
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+
+            string gameRecord;
+            List<string> gameStats = new List<string>();
+
+            //read in the info from an easy file and store into a list
+            using (StreamReader inFile = new StreamReader("../../easy/e1.txt"))
+            {
+                while ((gameRecord = inFile.ReadLine()) != null)
+                {
+                    gameStats.Add(gameRecord);
+                }
+            }
+
+            gameStats[0].Split('0');
+            char val = gameStats[0][0];
+            char val2 = gameStats[0][1];
+            char val3 = gameStats[0][2];
+
+            gameStats[1].Split('0');
+            char val4 = gameStats[1][0];
+            char val5 = gameStats[1][1];
+            char val6 = gameStats[1][2];
+
+            gameStats[2].Split('0');
+            char val7 = gameStats[2][0];
+            char val8 = gameStats[2][1];
+            char val9 = gameStats[2][2];
+
+
+            //addValuesToEasyBoard(val, val2, val3, val4, val5, val6, val7, val8, val9);
+
+            /*
+            //REMOVE THIS - just to test we have all info storing into the list
+            foreach(var i in gameStats)
+            {
+                output.AppendText("info: " + i + "\n");
+            }
+            */
+            Graphics graphics = e.Graphics;
+            if (isDown)
+            {
+                using (Pen gamePen = new Pen(Color.Black))
+                {
+                    gamePen.Width = 3;
+
+                    //draw the perimeter lines
+                    graphics.DrawLine(gamePen, 0, 0, W, 0);
+                    graphics.DrawLine(gamePen, 0, L, W, L);
+
+                    graphics.DrawLine(gamePen, 0, 0, 0, L);
+                    graphics.DrawLine(gamePen, W, 0, W, L);
+
+                    //TextBox txt = new TextBox();
+
+
+                    //if user wanted to play an easy game, then paint a 3x3 
+                    if (isEasyGame)
+                    {
+                        //easyPuzzleCell1textbox.Visible = true;
+                        //easyPuzzleCell2Textbox.Visible = true;
+
+                        //draw our vertical lines 
+                        graphics.DrawLine(gamePen, W / 3, 0, W / 3, L);
+                        graphics.DrawLine(gamePen, (2 * W / 3), 0, (2 * W / 3), L);
+
+                        //draw horizontal lines
+                        graphics.DrawLine(gamePen, 0, (L / 3), W, (L / 3));
+                        graphics.DrawLine(gamePen, 0, (2 * L / 3), W, (2 * L / 3));
+
+                        using (Font font1 = new Font("Times New Roman", 24, FontStyle.Bold, GraphicsUnit.Pixel))
+                        {
+                            /*
+                            for(int i = 1; i < 9; i++)
+                            {
+                                if (val != '0')
+                                {
+                                    PointF pointF1 = new PointF(W / 6, L / 6);
+                                    e.Graphics.DrawString(val.ToString(), font1, Brushes.Black, pointF1);
+                                }
+                            }
+                            */
+                            //FIRST ROW OF VALUES
+                            //cell 1
+                            if (val != '0')
+                            {
+                                //we have a value so print that to the board
+                                PointF pointF1 = new PointF(W / 6, L / 6);
+                                e.Graphics.DrawString(val.ToString(), font1, Brushes.Black, pointF1);
+                            }
+                            else
+                            {
+                                //it is zero, so user has to figure out what the value is
+                                Point pointF1 = new Point((W / 6) - 20, L / 6);
+                                TextBox txt = new TextBox();
+                                txt.Name = "easyPuzzleCell1";
+                                txt.Text = "";
+                                txt.Location = pointF1;
+                                txt.Height = 30;
+                                txt.Width = 30;
+                                //txt.Font.Size = 24;
+                                canvas.Controls.Add(txt);
+
+                                generatedEasyTextboxes.Add(txt);
+                            }
+                            //cell 2
+                            if (val2 != '0')
+                            {
+                                PointF pointF2 = new PointF(3 * (W / 6), (L / 6));
+                                e.Graphics.DrawString(val2.ToString(), font1, Brushes.Black, pointF2);
+                            }
+                            else
+                            {
+                                //it is zero, so user has to figure out what the value is
+                                Point pointF2 = new Point((3 * W / 6) - 20, L / 6);
+                                TextBox txt = new TextBox();
+                                txt.Name = "easyPuzzleCell2";
+                                txt.Text = "";
+                                txt.Location = pointF2;
+                                txt.Height = 30;
+                                txt.Width = 30;
+                                canvas.Controls.Add(txt);
+                                generatedEasyTextboxes.Add(txt);
+
+                            }
+                            //cell3 
+                            if (val3 != '0')
+                            {
+                                PointF pointF3 = new PointF(5 * (W / 6), (L / 6));
+                                e.Graphics.DrawString(val3.ToString(), font1, Brushes.Black, pointF3);
+                            }
+                            else
+                            {
+                                //it is zero, so user has to figure out what the value is
+                                Point pointF3 = new Point((5 * W / 6) - 20, L / 6);
+                                TextBox txt = new TextBox();
+                                txt.Name = "easyPuzzleCell3";
+                                txt.Text = "";
+                                txt.Location = pointF3;
+                                txt.Height = 30;
+                                txt.Width = 30;
+                                canvas.Controls.Add(txt);
+
+                                generatedEasyTextboxes.Add(txt);
+
+                            }
+
+                            //SECOND ROW OF VALUES
+                            //cell 4
+                            if (val4 != '0')
+                            {
+                                PointF pointF4 = new PointF(W / 6, (3 * L / 6));
+                                e.Graphics.DrawString(val4.ToString(), font1, Brushes.Black, pointF4);
+                            }
+                            else
+                            {
+                                //it is zero, so user has to figure out what the value is
+                                Point pointF4 = new Point(W / 6 - 20, (3 * L / 6));
+                                TextBox txt = new TextBox();
+                                txt.Name = "easyPuzzleCell4";
+                                txt.Text = "";
+                                txt.Location = pointF4;
+                                txt.Height = 30;
+                                txt.Width = 30;
+                                canvas.Controls.Add(txt);
+
+                                generatedEasyTextboxes.Add(txt);
+
+                            }
+                            //cell 5
+                            if (val5 != '0')
+                            {
+                                PointF pointF5 = new PointF(3 * (W / 6), (3 * L / 6));
+                                e.Graphics.DrawString(val5.ToString(), font1, Brushes.Black, pointF5);
+                            }
+                            else
+                            {
+
+                                //it is zero, so user has to figure out what the value is
+                                Point pointF5 = new Point(3 * (W / 6) - 20, (3 * L / 6));
+                                TextBox txt = new TextBox();
+                                txt.Name = "easyPuzzleCell5";
+                                txt.Text = "";
+                                txt.Location = pointF5;
+                                txt.Height = 30;
+                                txt.Width = 30;
+                                canvas.Controls.Add(txt);
+
+                                generatedEasyTextboxes.Add(txt);
+
+                            }
+                            //cell6 
+                            if (val6 != '0')
+                            {
+                                PointF pointF6 = new PointF(5 * (W / 6), (3 * L / 6));
+                                e.Graphics.DrawString(val6.ToString(), font1, Brushes.Black, pointF6);
+                            }
+                            else
+                            {
+
+                                //it is zero, so user has to figure out what the value is
+                                Point pointF6 = new Point((5 * W / 6) - 20, (3 * L / 6));
+                                TextBox txt = new TextBox();
+                                txt.Name = "easyPuzzleCell6";
+                                txt.Text = "";
+                                txt.Location = pointF6;
+                                txt.Height = 30;
+                                txt.Width = 30;
+                                canvas.Controls.Add(txt);
+
+                                generatedEasyTextboxes.Add(txt);
+
+                            }
+                            //THIRD ROW OF VALUES
+                            //cell 7
+                            if (val7 != '0')
+                            {
+                                PointF pointF7 = new PointF(W / 6, (5 * L / 6));
+                                e.Graphics.DrawString(val7.ToString(), font1, Brushes.Black, pointF7);
+                            }
+                            else
+                            {
+                                //it is zero, so user has to figure out what the value is
+                                Point pointF7 = new Point(W / 6 - 20, (5 * L / 6));
+                                TextBox txt = new TextBox();
+                                txt.Name = "easyPuzzleCell7";
+                                txt.Text = "";
+                                txt.Location = pointF7;
+                                txt.Height = 30;
+                                txt.Width = 30;
+                                canvas.Controls.Add(txt);
+
+                                generatedEasyTextboxes.Add(txt);
+
+                            }
+                            //cell 8
+                            if (val8 != '0')
+                            {
+                                PointF pointF8 = new PointF(3 * (W / 6), (5 * L / 6));
+                                e.Graphics.DrawString(val8.ToString(), font1, Brushes.Black, pointF8);
+                            }
+                            else
+                            {
+                                //it is zero, so user has to figure out what the value is
+                                Point pointF8 = new Point(3 * (W / 6) - 20, (5 * L / 6));
+                                TextBox txt = new TextBox();
+                                txt.Name = "easyPuzzleCell8";
+                                txt.Text = "";
+                                txt.Location = pointF8;
+                                txt.Height = 30;
+                                txt.Width = 30;
+                                canvas.Controls.Add(txt);
+
+                                generatedEasyTextboxes.Add(txt);
+
+                            }
+                            //cell 9
+                            if (val9 != '0')
+                            {
+                                PointF pointF9 = new PointF(5 * (W / 6), (5 * L / 6));
+                                e.Graphics.DrawString(val9.ToString(), font1, Brushes.Black, pointF9);
+                            }
+                            else
+                            {
+                                //it is zero, so user has to figure out what the value is
+                                Point pointF9 = new Point(5 * (W / 6) - 20, (5 * L / 6));
+                                TextBox txt = new TextBox();
+                                txt.Name = "easyPuzzleCell9";
+                                txt.Text = "";
+                                txt.Location = pointF9;
+                                txt.Height = 30;
+                                txt.Width = 30;
+                                canvas.Controls.Add(txt);
+
+                                generatedEasyTextboxes.Add(txt);
+
+                            }
+
+
+
+                        }
+                    }
+
+                    else if (isMediumGame)
+                    {
+                        // txt.Dispose();
+                        resetEasyPuzzleTextboxes();
+
+                        //draw our vertical lines s
+                        graphics.DrawLine(gamePen, W / 5, 0, W / 5, L);
+
+                        for (int i = 2; i < 5; i++)
+                        {
+                            graphics.DrawLine(gamePen, (i * W / 5), 0, (i * W / 5), L);
+                        }
+
+                        //draw horizontal lines
+                        graphics.DrawLine(gamePen, 0, (L / 5), W, (L / 5));
+
+                        for (int i = 2; i < 5; i++)
+                        {
+                            graphics.DrawLine(gamePen, 0, (i * L / 5), W, (i * L / 5));
+                        }
+                    }
+                    else if (isHardGame)
+                    {
+
+                        resetEasyPuzzleTextboxes();
+
+                        //draw our vertical lines 
+                        graphics.DrawLine(gamePen, W / 7, 0, W / 7, L);
+                        for (int i = 2; i < 7; i++)
+                        {
+                            graphics.DrawLine(gamePen, (i * W / 7), 0, (i * W / 7), L);
+                        }
+
+                        //draw horizontal lines
+                        graphics.DrawLine(gamePen, 0, (L / 7), W, (L / 7));
+                        for (int i = 2; i < 7; i++)
+                        {
+                            graphics.DrawLine(gamePen, 0, (i * L / 7), W, (i * L / 7));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void newGameButton_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDown = true;
+            canvas.Refresh();
+        }
+
+        private void newGameButton_MouseUp(object sender, MouseEventArgs e)
+        {
+            //reset all flags
+            isDown = false;
+            isEasyGame = false;
+            isMediumGame = false;
+            isHardGame = false;
+
+
+
+        }
+
+        private void resetEasyPuzzleTextboxes()
+        {
+
+            //remove the areas user can enter value for easy textboxes
+            for (int i = 0; i < generatedEasyTextboxes.Count; i++)
+            {
+                canvas.Controls.Remove(generatedEasyTextboxes[i]);
+            }
+            generatedEasyTextboxes.Clear();
+
+        }
+        private void addValuesToEasyBoard(char a, char b, char c, char d, char e, char f, char g, char h, char i)
+        {
+            //MAYBE ADD IT SO THAT USER CAN EDIT THE LABELS? OR ADD A TEXT AREA/ SET THE VALUES THAT ARE SET AND DISABLE EDITING
+            //USE DRAW STRING --> ON ASSIGNMENT DIRECTIONS
+
+
+            if (isDown)
+            {
+            }
+        }
+    }
+}
