a7c46541dc1699b2022f4a7d494092fb89130e99
Casey McDermott <casey0mcd@gmail.com>
2021-04-01 13:17:15 -0500

    Fixing Variables

--- JennyCasey_Assignment5/Form1.cs
+++ JennyCasey_Assignment5/Form1.cs
@@ -42,7 +42,6 @@ namespace JennyCasey_Assignment5
         private static bool isMediumBoard = false;
         private static bool isHardBoard = false;
         private static bool Hide_Board = false;
-        private static bool Paused = false;
         private static bool isDeletedValue = false;
         private static bool EasyRowCompleted = false;
         private static bool MediumRowCompleted = false;
@@ -57,6 +56,7 @@ namespace JennyCasey_Assignment5
         private static bool HardDiagnalCompleted1 = false;
         private static bool HardDiagnalCompleted2 = false;
         private static bool Complete = false;
+        private static bool Paused = false;
         private static bool EasySavedFlag = false;
         private static bool MediumSavedFlag = false;
         private static bool HardSavedFlag = false;
@@ -3932,7 +3932,7 @@ namespace JennyCasey_Assignment5
         //time and the board 
         private void completed_puzzle()
         {
-            if (EasyRowCompleted && EasyColCompleted && EasyDiagnalCompleted2)
+            if (EasyRowCompleted && EasyColCompleted && EasyDiagnalCompleted1 && EasyDiagnalCompleted2)
             {
                 tmrCounter.Enabled = false;
                 easy_itr++;
@@ -4030,7 +4030,7 @@ namespace JennyCasey_Assignment5
                 }
                 Complete = true; 
             }
-            else if (HardRowCompleted && HardDiagnalCompleted1 && HardDiagnalCompleted2)
+            else if (HardRowCompleted && HardColCompleted && HardDiagnalCompleted1 && HardDiagnalCompleted2)
             {
                 tmrCounter.Enabled = false;
                 hard_itr++;
